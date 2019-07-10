49ecd0cfc32cd01ee001c5c2c57708aeca01d7fc
tuffstudio <chris@tuff-studio.com>
2019-07-10 09:57:08 +0100

    custom leading

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
     extend: {
       colors: {
               'navy': '#0F1B2A',
-            }
+            },
+      lineHeight: {
+            sub: 0.1
+      }
     }
   },
   variants: {},
